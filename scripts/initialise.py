b9c409d53104cc252140edb51fae05436b8a9c84
Brett Kromkamp <brettkromkamp@gmail.com>
2022-07-31 12:40:17 +0200

    Initial commit

---
+++ scripts/initialise.py
@@ -0,0 +1,22 @@
+"""
+initialise.py file. Part of the Knowledge Graph project.
+
+July 31, 2022
+Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
+"""
+
+from topicdb.core.store.topicstore import TopicStore
+
+MAP_IDENTIFIER = 1
+USER_IDENTIFIER_1 = 1
+
+
+# Instantiate the topic store, create and subsequently populate a topic map
+store = TopicStore("init.db")
+store.create_database()
+store.create_map(USER_IDENTIFIER_1, "Test Map", "A map for testing purposes.")
+store.populate_map(MAP_IDENTIFIER, USER_IDENTIFIER_1)
+
+home_topic = store.get_topic(MAP_IDENTIFIER, "home")
+for base_name in home_topic.base_names:
+    print(base_name.name)
